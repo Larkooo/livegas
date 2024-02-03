6896c2521440317eda9bd97d0dad9f1844b66aaa
Nasr <larko236@outlook.com>
2024-02-03 13:53:21 -0500

    feat(grpc): grpc server with proto & services

--- packages/grpc/src/main.rs
+++ packages/grpc/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+mod services;
+
+pub mod hello_world {
+    use tonic::include_proto;
+
+    include_proto!("helloworld");
+}
+
+use tonic::transport::Server;
+
+use crate::hello_world::greeter_server::GreeterServer;
+use crate::services::greeter::MyGreeter;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
+    health_reporter
+        .set_serving::<GreeterServer<MyGreeter>>()
+        .await;
+
+    let addr = "[::1]:50051".parse().unwrap();
+    let greeter = MyGreeter::default();
+
+    println!("HealthServer + GreeterServer listening on {}", addr);
+
+    Server::builder()
+        .add_service(health_service)
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
---
+++ packages/grpc/src/services/greeter.rs
@@ -0,0 +1,21 @@
+use tonic::{Request, Response, Status};
+
+use crate::hello_world::{self, greeter_server::Greeter, HelloReply, HelloRequest};
+
+#[derive(Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request from {:?}", request.remote_addr());
+
+        let reply = hello_world::HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+        Ok(Response::new(reply))
+    }
+}
